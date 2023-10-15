f10734cae8fd989348556bb978cacfa426941b5a
Carlito <rody.castillo.tacuri@gmail.com>
2023-10-14 22:40:23 -0500

    chore[modules]: create new module of leads

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { LeadsController } from './modules/leads/leads.controller';
-import { LeadsService } from './modules/leads/leads.service';
 import { LeadsModule } from './modules/leads/leads.module';
 
 @Module({
   imports: [LeadsModule],
-  controllers: [AppController, LeadsController],
-  providers: [AppService, LeadsService],
 })
 export class AppModule {}
