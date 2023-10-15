79b96d0856d71ba47203017df3d40467c5231164
Carlito <rody.castillo.tacuri@gmail.com>
2023-10-14 22:28:53 -0500

    feat[app]: update project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LeadsController } from './leads/leads.controller';
-import { LeadsService } from './leads/leads.service';
+import { LeadsController } from './modules/leads/leads.controller';
+import { LeadsService } from './modules/leads/leads.service';
+import { LeadsModule } from './modules/leads/leads.module';
 
 @Module({
-  imports: [],
+  imports: [LeadsModule],
   controllers: [AppController, LeadsController],
   providers: [AppService, LeadsService],
 })
